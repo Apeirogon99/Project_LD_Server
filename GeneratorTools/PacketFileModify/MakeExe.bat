1777acc8d442f1efaa8eb18e0ee298db7b08c361
Apeirogon99 <Apeirogon99@gmail.com>
2023-02-25 21:35:11 +0900

    fixed untracked files

--- GeneratorTools/PacketFileModify/MakeExe.bat
+++
@@ -1,7 +0,0 @@
-pushd %~dp0
-pyinstaller --onefile PacketFileModify.py
-MOVE .\dist\PacketFileModify.exe ..\..\Protobuf\Generator\bin\PacketFileModify.exe
-@RD /S /Q .\build
-@RD /S /Q .\dist
-DEL /S /F /Q .\PacketFileModify.spec
-PAUSE
