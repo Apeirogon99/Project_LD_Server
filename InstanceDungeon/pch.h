2a9b67407b1aea3495e593ab6450dd2b4634a2c5
Apeirogon99 <Apeirogon99@gmail.com>
2023-08-31 23:53:18 +0900

    [gwanho] Update Indstnace Dugeon
    셋업

--- InstanceDungeon/pch.h
+++ InstanceDungeon/pch.h
@@ -1,5 +1,29 @@
 #pragma once
-class pch
-{
-};
 
+#define WIN32_LEAN_AND_MEAN
+
+#pragma warning ( disable : 4251 )
+
+//Protocol buffer
+#ifdef _DEBUG
+#pragma comment(lib, "Builds\\debug\\libprotobufd.lib")
+#pragma comment(lib, "Builds\\debug\\libprotobuf-lited.lib")
+#pragma comment(lib, "Builds\\debug\\libprotocd.lib")
+#else
+#pragma comment(lib, "Builds\\release\\libprotobuf.lib")
+#pragma comment(lib, "Builds\\release\\libprotobuf-lite.lib")
+#pragma comment(lib, "Builds\\release\\libprotoc.lib")
+#endif
+
+//Library
+#ifdef _DEBUG
+#pragma comment(lib, "Builds\\debug\\ApeirogonLibrary.lib")
+#pragma comment(lib, "Builds\\debug\\CommonLibrary.lib")
+#else
+#pragma comment(lib, "Builds\\release\\ApeirogonLibrary.lib")
+#pragma comment(lib, "Builds\\release\\CommonLibrary.lib")
+#endif
+
+#include "ApeirogonLibrary.h"
+#include "CommonLibaray.h"
+#include "GameLibaray.h"
